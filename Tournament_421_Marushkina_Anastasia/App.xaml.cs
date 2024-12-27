1617aab7448a24bfcf49c58ec286971d6f7e195a
User <User@DESKTOP-C03OQI7>
2024-12-27 09:39:15 +0300

    Add database

--- Tournament_421_Marushkina_Anastasia/App.xaml.cs
+++ Tournament_421_Marushkina_Anastasia/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Tournament_421_Marushkina_Anastasia.Bases;
 
 namespace Tournament_421_Marushkina_Anastasia
 {
@@ -13,5 +14,6 @@ namespace Tournament_421_Marushkina_Anastasia
     /// </summary>
     public partial class App : Application
     {
+        public static TournamentDB_421_Marushkina_AnastasiaEntities db = new TournamentDB_421_Marushkina_AnastasiaEntities();
     }
 }
